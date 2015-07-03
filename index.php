bbdafbf540adfbe1789953f9dfbf9dad7a89028d
Chelsea Stats <ChelseaStats@users.noreply.github.com>
2015-07-03 10:11:40 +0100

    wrap into a class and check if exists first

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * @package Editor extra privs
- * @author The Cellar Room Limited
- * @version 1.3.4
- */
 /*
 Plugin Name: Editor extra privs
 Version: 1.0.0
@@ -15,19 +10,33 @@ Copyright (c) 2015 by The Cellar Room Limited
 
 */
 
-function tcr_core_give_edit_theme_options( $caps ) {
-	
-	/* check if the user has the edit_pages capability */
-	if( ! empty( $caps[ 'edit_pages' ] ) ) {
-		
-		/* give the user the edit theme options capability */
-		$caps[ 'edit_theme_options' ] = true;
-		
-	}
-	
-	/* return the modified capabilities */
-	return $caps;
-	
-}
+defined( 'ABSPATH' ) or die();
 
-add_filter( 'user_has_cap', 'tcr_core_give_edit_theme_options' );
+/*************************************************************************/
+
+if ( ! class_exists( 'hesa_editor_extra_privs' ) ) :
+
+    class hesa_editor_extra_privs {
+    
+        function __construct() {
+            
+            add_filter( 'user_has_cap', array( $this, 'hesa_core_give_edit_theme_options' ));
+        }
+    
+        function hesa_core_give_edit_theme_options( $caps ) {
+        
+            /* check if the user has the edit_pages capability */
+            if( ! empty( $caps[ 'edit_pages' ] ) ) {
+        
+                /* give the user the edit theme options capability */
+                $caps[ 'edit_theme_options' ] = true;
+        
+            }
+        
+            /* return the modified capabilities */
+            return $caps;
+        
+        }
+    }
+
+endif;
